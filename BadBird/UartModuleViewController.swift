e929eee7c16cb6de49cf319de5bd85fce5f44e23
Wolf <wolf@iris.la>
2026-03-21 07:44:55 +0900

    Modernize project to current Apple standards
    Fix broken file paths in pbxproj, add scene lifecycle (SceneDelegate),
    update build settings (deployment target 15.0, Xcode 14+ compat),
    fix storyboard module name Basic_Chat→BadBird, modernize Info.plist
    with scene manifest, consolidate AppIcon to single 1024x1024 universal
    format, and remove unused files/assets.

--- BadBird/UartModuleViewController.swift
+++ BadBird/UartModuleViewController.swift
@@ -6,232 +6,168 @@
 //  Copyright © 2016 Vanguard Logic LLC. All rights reserved.
 //
 
-
-
-
-
 import UIKit
 import CoreBluetooth
 
-class UartModuleViewController: UIViewController, CBPeripheralManagerDelegate, UITextViewDelegate, UITextFieldDelegate {
-    
-    //UI
+class UartModuleViewController: UIViewController, UITextViewDelegate, UITextFieldDelegate {
+
+    // MARK: - UI
     @IBOutlet weak var baseTextView: UITextView!
     @IBOutlet weak var sendButton: UIButton!
     @IBOutlet weak var inputTextField: UITextField!
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var switchUI: UISwitch!
-    //Data
-    var peripheralManager: CBPeripheralManager?
+
+    // MARK: - Data
     var peripheral: CBPeripheral!
-    private var consoleAsciiText:NSAttributedString? = NSAttributedString(string: "")
-    
-    
+    private var consoleAsciiText = NSMutableAttributedString()
+    private var notificationObserver: (any NSObjectProtocol)?
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        self.navigationItem.backBarButtonItem = UIBarButtonItem(title:"Back", style:.plain, target:nil, action:nil)
-        self.baseTextView.delegate = self
-        self.inputTextField.delegate = self
-        //Base text view setup
-        self.baseTextView.layer.borderWidth = 3.0
-        self.baseTextView.layer.borderColor = UIColor.blue.cgColor
-        self.baseTextView.layer.cornerRadius = 3.0
-        self.baseTextView.text = ""
-        //Input Text Field setup
-        self.inputTextField.layer.borderWidth = 2.0
-        self.inputTextField.layer.borderColor = UIColor.blue.cgColor
-        self.inputTextField.layer.cornerRadius = 3.0
-        //Create and start the peripheral manager
-        peripheralManager = CBPeripheralManager(delegate: self, queue: nil)
-        //-Notification for updating the text view with incoming text
+
+        navigationItem.backBarButtonItem = UIBarButtonItem(title: "Back", style: .plain, target: nil, action: nil)
+        baseTextView.delegate = self
+        inputTextField.delegate = self
+
+        baseTextView.layer.borderWidth = 3.0
+        baseTextView.layer.borderColor = UIColor.blue.cgColor
+        baseTextView.layer.cornerRadius = 3.0
+        baseTextView.text = ""
+
+        inputTextField.layer.borderWidth = 2.0
+        inputTextField.layer.borderColor = UIColor.blue.cgColor
+        inputTextField.layer.cornerRadius = 3.0
+
         updateIncomingData()
     }
-    
+
     override func viewDidAppear(_ animated: Bool) {
-        self.baseTextView.text = ""
-        
-        
+        super.viewDidAppear(animated)
+        baseTextView.text = ""
     }
-    
+
     override func viewDidDisappear(_ animated: Bool) {
-        // peripheralManager?.stopAdvertising()
-        // self.peripheralManager = nil
         super.viewDidDisappear(animated)
-        NotificationCenter.default.removeObserver(self)
-        
-    }
-    
-    func updateIncomingData () {
-        NotificationCenter.default.addObserver(forName: NSNotification.Name(rawValue: "Notify"), object: nil , queue: nil){
-            notification in
-            let appendString = "\n"
-            let myFont = UIFont(name: "Helvetica Neue", size: 15.0)
-            let myAttributes2 = [NSFontAttributeName: myFont!, NSForegroundColorAttributeName: UIColor.red]
-            let attribString = NSAttributedString(string: "[Incoming]: " + (characteristicASCIIValue as String) + appendString, attributes: myAttributes2)
-            let newAsciiText = NSMutableAttributedString(attributedString: self.consoleAsciiText!)
-            self.baseTextView.attributedText = NSAttributedString(string: characteristicASCIIValue as String , attributes: myAttributes2)
-            
-            newAsciiText.append(attribString)
-            
-            self.consoleAsciiText = newAsciiText
-            self.baseTextView.attributedText = self.consoleAsciiText
-            
+        if let observer = notificationObserver {
+            NotificationCenter.default.removeObserver(observer)
+            notificationObserver = nil
         }
     }
-    
+
+    func updateIncomingData() {
+        notificationObserver = NotificationCenter.default.addObserver(
+            forName: NSNotification.Name(rawValue: "Notify"),
+            object: nil,
+            queue: .main
+        ) { [weak self] _ in
+            guard let self else { return }
+
+            let appendString = "\n"
+            let myFont = UIFont(name: "Helvetica Neue", size: 15.0) ?? UIFont.systemFont(ofSize: 15.0)
+            let attributes: [NSAttributedString.Key: Any] = [
+                .font: myFont,
+                .foregroundColor: UIColor.red
+            ]
+            let attribString = NSAttributedString(
+                string: "[Incoming]: " + characteristicASCIIValue + appendString,
+                attributes: attributes
+            )
+            self.consoleAsciiText.append(attribString)
+            self.baseTextView.attributedText = self.consoleAsciiText
+        }
+    }
+
     @IBAction func clickSendAction(_ sender: AnyObject) {
         outgoingData()
-        
     }
-    
-    
-    
-    func outgoingData () {
+
+    func outgoingData() {
         let appendString = "\n"
-        
-        let inputText = inputTextField.text
-        
-        let myFont = UIFont(name: "Helvetica Neue", size: 15.0)
-        let myAttributes1 = [NSFontAttributeName: myFont!, NSForegroundColorAttributeName: UIColor.blue]
-        
-        writeValue(data: inputText!)
-        lock = !lock
-        let attribString = NSAttributedString(string: "[Outgoing]: " + inputText! + appendString, attributes: myAttributes1)
-        let newAsciiText = NSMutableAttributedString(attributedString: self.consoleAsciiText!)
-        newAsciiText.append(attribString)
-        
-        consoleAsciiText = newAsciiText
+        let inputText = inputTextField.text ?? ""
+
+        let myFont = UIFont(name: "Helvetica Neue", size: 15.0) ?? UIFont.systemFont(ofSize: 15.0)
+        let attributes: [NSAttributedString.Key: Any] = [
+            .font: myFont,
+            .foregroundColor: UIColor.blue
+        ]
+
+        sendCommand(lock: isLocked)
+        isLocked = !isLocked
+
+        let attribString = NSAttributedString(
+            string: "[Outgoing]: " + inputText + appendString,
+            attributes: attributes
+        )
+        consoleAsciiText.append(attribString)
         baseTextView.attributedText = consoleAsciiText
-        //erase what's in the text field
         inputTextField.text = ""
-        
     }
-    
-    // Write functions
-    func writeValue(data: String){
-//        let valueString = (data as NSString).data(using: String.Encoding.utf8.rawValue)
-        //change the "data" to valueString
-       // 55 aa 03 20 03 70 01 68 ff
-        
-        if (lock)
-        {
-            let sendBytes : [UInt8] = [ 0x55, 0xaa, 0x03, 0x20, 0x03, 0x70, 0x01, 0x68, 0xff]
-            let valueString = Data(bytes:sendBytes)
-            
-            if let blePeripheral = blePeripheral{
-                if let txCharacteristic = txCharacteristic {
-                    blePeripheral.writeValue(valueString, for: txCharacteristic, type: CBCharacteristicWriteType.withoutResponse)
-                }
-            }
-            
-        }
-        else
-        {
-            let sendBytes : [UInt8] = [ 0x55, 0xaa, 0x03, 0x20, 0x03, 0x71, 0x01, 0x67, 0xff]
-            let valueString = Data(bytes:sendBytes)
-            
-            if let blePeripheral = blePeripheral{
-                if let txCharacteristic = txCharacteristic {
-                    blePeripheral.writeValue(valueString, for: txCharacteristic, type: CBCharacteristicWriteType.withoutResponse)
-                }
-            }
-        }
 
-    }
-    
-    
+    // MARK: - BLE Write
 
-    
-    func writeCharacteristic(val: Int8){
-        var val = val
-        let ns = NSData(bytes: &val, length: MemoryLayout<Int8>.size)
-        blePeripheral!.writeValue(ns as Data, for: txCharacteristic!, type: CBCharacteristicWriteType.withResponse)
+    func sendCommand(lock: Bool) {
+        let bytes = lock ? Mi365Command.lock : Mi365Command.unlock
+        let data = Data(bytes)
+
+        guard let peripheral = blePeripheral,
+              let characteristic = txCharacteristic else {
+            print("BLE not connected — cannot send command")
+            return
+        }
+        peripheral.writeValue(data, for: characteristic, type: .withoutResponse)
     }
-    
-    
-    
-    //MARK: UITextViewDelegate methods
+
+    func writeCharacteristic(val: Int8) {
+        guard let peripheral = blePeripheral,
+              let characteristic = txCharacteristic else {
+            print("BLE not connected — cannot write characteristic")
+            return
+        }
+        var value = val
+        let data = Data(bytes: &value, count: MemoryLayout<Int8>.size)
+        peripheral.writeValue(data, for: characteristic, type: .withResponse)
+    }
+
+    // MARK: - UITextViewDelegate
+
     func textViewShouldBeginEditing(_ textView: UITextView) -> Bool {
         if textView === baseTextView {
-            //tapping on consoleview dismisses keyboard
             inputTextField.resignFirstResponder()
             return false
         }
         return true
     }
-    
+
     func textFieldDidBeginEditing(_ textField: UITextField) {
-        scrollView.setContentOffset(CGPoint(x:0, y:250), animated: true)
+        scrollView.setContentOffset(CGPoint(x: 0, y: 250), animated: true)
     }
-    
+
     func textFieldDidEndEditing(_ textField: UITextField) {
-        scrollView.setContentOffset(CGPoint(x:0, y:0), animated: true)
+        scrollView.setContentOffset(CGPoint(x: 0, y: 0), animated: true)
     }
-    
-    func peripheralManagerDidUpdateState(_ peripheral: CBPeripheralManager) {
-        if peripheral.state == .poweredOn {
-            return
-        }
-        print("Peripheral manager is running")
-    }
-    
-    //Check when someone subscribe to our characteristic, start sending the data
-    func peripheralManager(_ peripheral: CBPeripheralManager, central: CBCentral, didSubscribeTo characteristic: CBCharacteristic) {
-        print("Device subscribe to characteristic")
-    }
-    
-    //This on/off switch sends a value of 1 and 0 to the Arduino
-    //This can be used as a switch or any thing you'd like
+
+    // MARK: - Switch Action
+
     @IBAction func switchAction(_ sender: Any) {
         if switchUI.isOn {
-            print("On ")
-//            if (lock)
-//            {
-                let sendBytes : [UInt8] = [ 0x55, 0xaa, 0x03, 0x20, 0x03, 0x70, 0x01, 0x68, 0xff]
-                let valueString = Data(bytes:sendBytes)
-                
-                if let blePeripheral = blePeripheral{
-                    if let txCharacteristic = txCharacteristic {
-                        blePeripheral.writeValue(valueString, for: txCharacteristic, type: CBCharacteristicWriteType.withoutResponse)
-                    }
-                }
-                
-            
+            print("On")
+            sendCommand(lock: true)
             writeCharacteristic(val: 1)
-        }
-        else
-        {
+        } else {
             print("Off")
-            let sendBytes : [UInt8] = [ 0x55, 0xaa, 0x03, 0x20, 0x03, 0x71, 0x01, 0x67, 0xff]
-            let valueString = Data(bytes:sendBytes)
-            
-            if let blePeripheral = blePeripheral{
-                if let txCharacteristic = txCharacteristic {
-                    blePeripheral.writeValue(valueString, for: txCharacteristic, type: CBCharacteristicWriteType.withoutResponse)
-                }
-            }
-            
+            sendCommand(lock: false)
             writeCharacteristic(val: 0)
-            print(writeCharacteristic)
         }
     }
-    
+
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        self.view.endEditing(true)
+        view.endEditing(true)
     }
-    
+
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
         outgoingData()
-        return(true)
-    }
-    
-    func peripheralManagerDidStartAdvertising(_ peripheral: CBPeripheralManager, error: Error?) {
-        if let error = error {
-            print("\(error)")
-            return
-        }
+        return true
     }
 }
-
