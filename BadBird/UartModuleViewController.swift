3633fb7c27539fb2e70d2e53ab4a3835cc946e12
Wolf <wolf@iris.la>
2026-03-21 07:56:37 +0900

    Apply Xcode 26.3 project modernization and MainActor isolation fix
    Xcode auto-updated build settings (new warnings, parallel builds,
    script sandboxing) and UartModuleViewController now wraps its
    notification closure body in MainActor.assumeIsolated for Swift 6.

--- BadBird/UartModuleViewController.swift
+++ BadBird/UartModuleViewController.swift
@@ -61,20 +61,22 @@ class UartModuleViewController: UIViewController, UITextViewDelegate, UITextFiel
             object: nil,
             queue: .main
         ) { [weak self] _ in
-            guard let self else { return }
+            MainActor.assumeIsolated {
+                guard let self else { return }
 
-            let appendString = "\n"
-            let myFont = UIFont(name: "Helvetica Neue", size: 15.0) ?? UIFont.systemFont(ofSize: 15.0)
-            let attributes: [NSAttributedString.Key: Any] = [
-                .font: myFont,
-                .foregroundColor: UIColor.red
-            ]
-            let attribString = NSAttributedString(
-                string: "[Incoming]: " + characteristicASCIIValue + appendString,
-                attributes: attributes
-            )
-            self.consoleAsciiText.append(attribString)
-            self.baseTextView.attributedText = self.consoleAsciiText
+                let appendString = "\n"
+                let myFont = UIFont(name: "Helvetica Neue", size: 15.0) ?? UIFont.systemFont(ofSize: 15.0)
+                let attributes: [NSAttributedString.Key: Any] = [
+                    .font: myFont,
+                    .foregroundColor: UIColor.red
+                ]
+                let attribString = NSAttributedString(
+                    string: "[Incoming]: " + characteristicASCIIValue + appendString,
+                    attributes: attributes
+                )
+                self.consoleAsciiText.append(attribString)
+                self.baseTextView.attributedText = self.consoleAsciiText
+            }
         }
     }
 
